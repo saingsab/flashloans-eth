1f74c48677569f9a89d26f104b963aa50e35c0ff
Saing Sab <saing.sab@gmail.com>
2021-03-24 13:45:00 +0700

    correction

--- contracts/Flashloans.sol
+++ contracts/Flashloans.sol
@@ -3,14 +3,43 @@ pragma experimental ABIEncoderV2;
 
 import "@studydefi/money-legos/dydx/contracts/DydxFlashloanBase.sol";
 import "@studydefi/money-legos/dydx/contracts/ICallee.sol";
+import {
+    KyberNetworkProxy as IKyberNetworkProxy
+} from "@studydefi/money-legos/kyber/contracts/KyberNetworkProxy.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./IUniswapV2Router02.sol";
+import "./IWeth.sol";
 
 contract Flashloan is ICallee, DydxFlashloanBase {
     enum Direction {KyberToUniswap, UniswapToKyber}
     struct ArbInfo {
         Direction direction;
         uint256 repayAmount;
+    }
+
+    event NewArbitrage(Direction direction, uint256 profit, uint256 date);
+
+    IKyberNetworkProxy kyber;
+    IUniswapV2Router02 uniswap;
+    IWeth weth;
+    IERC20 dai;
+    address beneficiary;
+    address constant KYBER_ETH_ADDRESS =
+        0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
+
+    constructor(
+        address kyberAddress,
+        address uniswapAddress,
+        address wethAddress,
+        address daiAddress,
+        address beneficiaryAddress
+    ) public {
+        kyber = IKyberNetworkProxy(kyberAddress);
+        uniswap = IUniswapV2Router02(uniswapAddress);
+        weth = IWeth(wethAddress);
+        dai = IERC20(daiAddress);
+        beneficiary = beneficiaryAddress;
     }
 
     // This is the function that will be called postLoan
@@ -21,9 +50,70 @@ contract Flashloan is ICallee, DydxFlashloanBase {
         bytes memory data
     ) public {
         ArbInfo memory arbInfo = abi.decode(data, (ArbInfo));
+        uint256 balanceDai = dai.balanceOf(address(this));
+
+        if (arbInfo.direction == Direction.KyberToUniswap) {
+            //Buy ETH on Kyber
+            dai.approve(address(kyber), balanceDai);
+            (uint256 expectedRate, ) =
+                kyber.getExpectedRate(
+                    dai,
+                    IERC20(KYBER_ETH_ADDRESS),
+                    balanceDai
+                );
+            kyber.swapTokenToEther(dai, balanceDai, expectedRate);
+
+            //Sell ETH on Uniswap
+            address[] memory path = new address[](2);
+            path[0] = address(weth);
+            path[1] = address(dai);
+            uint256[] memory minOuts =
+                uniswap.getAmountsOut(address(this).balance, path);
+            uniswap.swapExactETHForTokens.value(address(this).balance)(
+                minOuts[1],
+                path,
+                address(this),
+                now
+            );
+        } else {
+            //Buy ETH on Uniswap
+            dai.approve(address(uniswap), balanceDai);
+            address[] memory path = new address[](2);
+            path[0] = address(dai);
+            path[1] = address(weth);
+            uint256[] memory minOuts = uniswap.getAmountsOut(balanceDai, path);
+            uniswap.swapExactTokensForETH(
+                balanceDai,
+                minOuts[1],
+                path,
+                address(this),
+                now
+            );
+
+            //Sell ETH on Kyber
+            (uint256 expectedRate, ) =
+                kyber.getExpectedRate(
+                    IERC20(KYBER_ETH_ADDRESS),
+                    dai,
+                    address(this).balance
+                );
+            kyber.swapEtherToToken.value(address(this).balance)(
+                dai,
+                expectedRate
+            );
+        }
+
+        require(
+            dai.balanceOf(address(this)) >= arbInfo.repayAmount,
+            "Not enough funds to repay dydx loan!"
+        );
+
+        uint256 profit = dai.balanceOf(address(this)) - arbInfo.repayAmount;
+        dai.transfer(beneficiary, profit);
+        emit NewArbitrage(arbInfo.direction, profit, now);
     }
 
-    function initiateFlashLoan(
+    function initiateFlashloan(
         address _solo,
         address _token,
         uint256 _amount,
--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -72,6 +74,14 @@ module.exports = {
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
     // }
+    kovan: {
+      provider: () => new HDWalletProvider(
+        process.env.PRIVATE_KEY,
+        process.env.INFURA_URL_KOVAN
+      ),
+      network_id: 42,
+    },
+
   },
 
   // Set default mocha options here, use special reporters etc.
